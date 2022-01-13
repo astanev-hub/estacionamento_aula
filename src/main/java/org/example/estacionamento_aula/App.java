95ce912b64efe65d92fbfe024f7a18ba8b7d0710
Alexandre Stanev <astanev@hotmail.com>
2022-01-13 19:57:50 -0300

    Commit inicial

---
+++ src/main/java/org/example/estacionamento_aula/App.java
@@ -0,0 +1,13 @@
+package org.example.estacionamento_aula;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
